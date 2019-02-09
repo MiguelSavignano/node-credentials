20c334c5b9b9457467fcc6180cceb5a4da182e96
Miguel Savignano <migue.masx@gmail.com>
2019-02-09 19:00:12 +0100

    test encrypt file

--- test/node-vault.spec.js
+++ test/node-vault.spec.js
@@ -3,13 +3,19 @@ let NODE_MASTER_KEY = "8aa93853b3ff01c5b5447529a9c33cb9";
 var fs = require("fs");
 require("./helpers/matchers");
 
-test("encrypt", async () => {
+describe("node-vault", () => {
   let credentialsPath = __dirname + "/examples/credentials.json";
-  expect(
-    await nodeVault.encrypt({
-      keyValue: NODE_MASTER_KEY,
-      credentialsFilePath: credentialsPath
-    })
-  ).toEqual(`${credentialsPath}.enc`);
-  expect(fs.readFileSync(`${credentialsPath}.enc`)).validEncrypted();
+
+  afterEach(() => {
+    fs.unlinkSync(`${credentialsPath}.enc`);
+  });
+  test("encrypt", async () => {
+    expect(
+      await nodeVault.encrypt({
+        keyValue: NODE_MASTER_KEY,
+        credentialsFilePath: credentialsPath
+      })
+    ).toEqual(`${credentialsPath}.enc`);
+    expect(fs.readFileSync(`${credentialsPath}.enc`)).validEncrypted();
+  });
 });
