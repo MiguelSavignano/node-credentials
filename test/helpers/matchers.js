aa9b1edc86fde7bd57294e0d426f6bfeb91cb5b6
Miguel Savignano <migue.masx@gmail.com>
2019-02-09 19:05:04 +0100

    refactor test matcher

--- test/helpers/matchers.js
+++ test/helpers/matchers.js
@@ -1,8 +1,22 @@
+var fs = require("fs");
+
+const validEncrypted = value => {
+  return value.length === 70;
+};
 expect.extend({
   async validEncrypted(value) {
     return {
-      pass: value.length === 70,
+      pass: validEncrypted(value),
       message: () => "valid encrypted"
+    };
+  }
+});
+
+expect.extend({
+  async validEncryptedFile(value) {
+    return {
+      pass: validEncrypted(fs.readFileSync(value)),
+      message: () => "valid encrypted file"
     };
   }
 });
--- test/node-vault.spec.js
+++ test/node-vault.spec.js
@@ -1,7 +1,7 @@
-const nodeVault = require("../node-vault");
-let NODE_MASTER_KEY = "8aa93853b3ff01c5b5447529a9c33cb9";
 var fs = require("fs");
+const nodeVault = require("../node-vault");
 require("./helpers/matchers");
+let NODE_MASTER_KEY = "8aa93853b3ff01c5b5447529a9c33cb9";
 
 describe("node-vault", () => {
   let credentialsPath = __dirname + "/examples/credentials.json";
@@ -9,13 +9,13 @@ describe("node-vault", () => {
   afterEach(() => {
     fs.unlinkSync(`${credentialsPath}.enc`);
   });
+
   test("encrypt", async () => {
     expect(
       await nodeVault.encrypt({
         keyValue: NODE_MASTER_KEY,
         credentialsFilePath: credentialsPath
       })
-    ).toEqual(`${credentialsPath}.enc`);
-    expect(fs.readFileSync(`${credentialsPath}.enc`)).validEncrypted();
+    ).validEncryptedFile();
   });
 });
