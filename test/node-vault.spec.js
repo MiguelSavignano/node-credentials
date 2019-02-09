26d2518af97dbbc01647c2cd8b7e57dc5a59df37
Miguel Savignano <migue.masx@gmail.com>
2019-02-09 19:21:51 +0100

    .

--- test/node-vault.spec.js
+++ test/node-vault.spec.js
@@ -3,22 +3,22 @@ const nodeVault = require("../node-vault");
 require("./helpers/matchers");
 let NODE_MASTER_KEY = "8aa93853b3ff01c5b5447529a9c33cb9";
 
-// describe("node-vault", () => {
-//   let credentialsPath = __dirname + "/examples/encrypt/credentials.json";
+describe("node-vault", () => {
+  let credentialsPath = __dirname + "/examples/encrypt/credentials.json";
 
-//   afterEach(() => {
-//     fs.unlinkSync(`${credentialsPath}.enc`);
-//   });
+  afterEach(() => {
+    fs.unlinkSync(`${credentialsPath}.enc`);
+  });
 
-//   test("encrypt", async () => {
-//     expect(
-//       await nodeVault.encrypt({
-//         keyValue: NODE_MASTER_KEY,
-//         credentialsFilePath: credentialsPath
-//       })
-//     ).validEncryptedFile();
-//   });
-// });
+  test("encrypt", async () => {
+    expect(
+      await nodeVault.encrypt({
+        keyValue: NODE_MASTER_KEY,
+        credentialsFilePath: credentialsPath
+      })
+    ).validEncryptedFile();
+  });
+});
 
 describe("node-vault", () => {
   let credentialsPath = __dirname + "/examples/decrypt/credentials.json";
