b450fbc7ab73436d7195ef14140549c4967fee16
Miguel Savignano <migue.masx@gmail.com>
2020-08-30 21:35:01 +0200

    encrypt any json file

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ const commandLineUsage = require('command-line-usage');
 const Vault = require('./vault').Vault;
 
 const init = ({ path }) => {
-  const vault = new Vault({ credentialsFilePath: `${path}/credentials.json` });
+  const vault = new Vault({ credentialsFilePath: path });
   if (fs.existsSync(`${vault.credentialsFilePath}.enc`)) {
     console.log('Warning credentials.json.enc exists, ensure decrypt file before generate new key');
   } else if (fs.existsSync(`${vault.credentialsFilePath}.key`)) {
@@ -20,9 +20,7 @@ const init = ({ path }) => {
 };
 
 const encrypt = ({ path }) => {
-  const vault = new Vault({
-    credentialsFilePath: `${path}/credentials.json`,
-  });
+  const vault = new Vault({ credentialsFilePath: path });
   if (fs.existsSync(`${vault.credentialsFilePath}.key`) || process.env.NODE_MASTER_KEY) {
     vault
       .encryptFile()
@@ -30,16 +28,14 @@ const encrypt = ({ path }) => {
         fs.unlinkSync(`${vault.credentialsFilePath}`);
         fs.unlinkSync(`${vault.credentialsFilePath}.iv`);
       })
-      .catch((error) => {});
+      .catch((error) => console.error(error));
   } else {
     console.log('Warning credentials.json.key not exists, create new key with init');
   }
 };
 
 const edit = ({ path }) => {
-  const vault = new Vault({
-    credentialsFilePath: `${path}/credentials.json`,
-  });
+  const vault = new Vault({ credentialsFilePath: path });
   if (!fs.existsSync(`${vault.credentialsFilePath}.enc`)) {
     console.log('Error credentials.json.enc not exists');
   } else {
