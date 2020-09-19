ebc9a006274370b0a23d4a7d36cdf551af3f2330
Miguel Savignano <migue.masx@gmail.com>
2020-09-19 03:00:33 +0200

    use the same credentials file to encrypt or decrypt

--- src/vault.js
+++ src/vault.js
@@ -40,12 +40,12 @@ class Vault {
     this._credentialsEnv = get(credentials, this.nodeEnv, {});
   }
 
-  config({ keyValue, path } = {}) {
+  config({ path } = {}) {
     if (path) {
       this.credentialsFilePath = path;
     }
-    const key = keyValue || this.getMasterKey();
-    const text = fs.readFileSync(`${this.credentialsFilePath}.enc`, 'utf8');
+    const key = this.getMasterKey();
+    const text = fs.readFileSync(`${this.credentialsFilePath}`, 'utf8');
     const [credentialsText, iv] = this.decryptFnc(key, text);
     const credentialsTextRendered = render(credentialsText);
     const credentials = JSON.parse(credentialsTextRendered);
@@ -54,8 +54,8 @@ class Vault {
     return credentials;
   }
 
-  async encryptFile({ keyValue } = {}) {
-    const key = keyValue || this.getMasterKey();
+  async encryptFile() {
+    const masterKey = this.getMasterKey(true);
     const text = fs.readFileSync(this.credentialsFilePath, 'utf8').trim();
     let iv;
     try {
@@ -63,15 +63,19 @@ class Vault {
     } catch {
       iv = null;
     }
-    const cipherBundle = await this.encryptFnc(key, text, iv);
-    fs.writeFileSync(`${this.credentialsFilePath}.enc`, cipherBundle);
-    return `${this.credentialsFilePath}.enc`;
+    const cipherBundle = await this.encryptFnc(masterKey, text, iv);
+    fs.writeFileSync(`${this.credentialsFilePath}`, cipherBundle);
+    try {
+      fs.unlinkSync(`${this.credentialsFilePath}.iv`, 'utf8');
+    } catch {}
+
+    return `${this.credentialsFilePath}`;
   }
 
-  editCredentials({ keyValue } = {}) {
-    const key = keyValue || this.getMasterKey();
-    const text = fs.readFileSync(`${this.credentialsFilePath}.enc`, 'utf8');
-    const [decryptCredentials, iv] = this.decryptFnc(key, text);
+  decryptFile() {
+    const masterKey = this.getMasterKey(true);
+    const text = fs.readFileSync(`${this.credentialsFilePath}`, 'utf8');
+    const [decryptCredentials, iv] = this.decryptFnc(masterKey, text);
     fs.writeFileSync(`${this.credentialsFilePath}`, decryptCredentials, 'utf8');
     fs.writeFileSync(`${this.credentialsFilePath}.iv`, iv, 'utf8');
     return this.credentialsFilePath;
@@ -83,14 +87,17 @@ class Vault {
     return newKey;
   }
 
-  getMasterKey() {
+  getMasterKey(force = true) {
     try {
       return (
         this.masterKey ||
         process.env.NODE_MASTER_KEY ||
         fs.readFileSync(`${this.credentialsFilePath}.key`, 'utf8').trim()
       );
-    } catch {
+    } catch (e) {
+      if (force) {
+        throw new Error('Missing master key, check .key file or the NODE_MASTER_KEY enviroment variable');
+      }
       return undefined;
     }
   }
