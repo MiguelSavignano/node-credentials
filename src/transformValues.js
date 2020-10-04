d41e47938fa038c2ee47c64a67a99cc065fb3eda
Miguel Savignano <migue.masx@gmail.com>
2020-10-04 02:21:18 +0200

    transform yaml doc values

--- src/transformValues.js
+++ src/transformValues.js
@@ -1,3 +1,5 @@
+const YAML = require('yaml');
+
 function isObject(variable) {
   return variable !== undefined && variable !== null && variable.constructor === Object;
 }
@@ -41,4 +43,50 @@ const transformValues = (object, fnc) => {
   }, {});
 };
 
-module.exports = { transformValues, isPromiseOrAsync, isObject, resolveSubTypes };
+const yAMLDocResolveSubTypes = (item) => {
+  if (item.type === "MAP") { // item Array
+    return item.items.forEach(item2 => {
+      return yAMLDocResolveSubTypes(item2)
+    })
+  }
+
+  if (item.value.type === "QUOTE_DOUBLE") { // value String
+    item.value.value = `${item.value.value} encrpted`
+    return true
+  }
+
+  if (item.value.type === "PLAIN") { // value number
+    item.value.value = `${item.value.value} encrypted`
+    return true
+  }
+
+  if (item.value.type === "MAP") { // value object
+    return item.value.items.forEach(item2 => {
+      return yAMLDocResolveSubTypes(item2)
+    })
+  }
+
+  if (item.value.type === "ALIAS") {
+    return true
+  }
+  if (item.value.type === "SEQ") {
+    return item.value.items.forEach(item2 => {
+      yAMLDocResolveSubTypes(item2)
+    })
+  }
+  // console.log(item.value.type)
+}
+
+// https://eemeli.org/yaml/#documents
+// Document.Parsed
+const transformYAMLDocValues = (text, fnc) => {
+  const doc = YAML.parseDocument(text, { merge: false });
+  return new Promise(async (resolve, reject) => {
+    doc.contents.items.forEach(item => {
+      yAMLDocResolveSubTypes(item)
+    })
+    resolve(doc);
+  });
+};
+
+module.exports = { transformValues, transformYAMLDocValues, isPromiseOrAsync, isObject, resolveSubTypes };
