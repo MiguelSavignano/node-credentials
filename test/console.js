198373de23379653778ae0daaf87824e2bb3d914
Miguel Savignano <migue.masx@gmail.com>
2020-10-24 22:46:51 +0200

    skip encrypt with yaml comment

---
+++ test/console.js
@@ -0,0 +1,16 @@
+const core = require('../src/core');
+
+const yaml = `
+public_key: value #no-encrypt
+secret: value2
+data:
+ - data1
+ - data2 # no-encrypt
+`;
+
+async function run() {
+  const result = await core.encryptYAML('NODE_MASTER_KEY', yaml);
+  console.log(result)
+}
+
+run()
